d34d587d1aaf7bbcf7520d7a02c666d688cab5d8
Anton Bykov <tigrawap@gmail.com>
2024-10-20 11:20:08 +0300

    feat: add condition for JoinIpsSet
    Cluster reconcile should periodically/on-events ensure correct list of
    join ips

--- api/v1alpha1/condition/conditions.go
+++ api/v1alpha1/condition/conditions.go
@@ -14,6 +14,7 @@ const (
 	CondS3ClusterCreated            = "CondS3ClusterCreated" // not pre-set on purpose, as optional
 	CondPodsReady                   = "PodsReady"
 	CondClusterCreated              = "ClusterCreated"
+	CondJoinIpsSet                  = "JoinIpsSet"
 	CondDrivesAdded                 = "DrivesAdded"
 	CondIoStarted                   = "IoStarted"
 	CondJoinedCluster               = "JoinedCluster"
