a4aa9f7ec0a53053ad13a80d3c9d22f5c6017675
Anton Bykov <tigrawap@gmail.com>
2024-11-11 13:04:38 +0200

    feat: add stripe size and data protection level configs

--- api/v1alpha1/wekacluster_types.go
+++ api/v1alpha1/wekacluster_types.go
@@ -182,7 +182,9 @@ type WekaClusterSpec struct {
 	// A hot spare is reserved capacity designed to handle data rebuilds while maintaining the system's net capacity, even in the event of failure domains being lost
 	// See: https://docs.weka.io/weka-system-overview/ssd-capacity-management#hot-spare
 	// +kubebuilder:default=0
-	HotSpare int `json:"hotSpare,omitempty"`
+	HotSpare        int `json:"hotSpare,omitempty"`
+	RedundancyLevel int `json:"redundancyLevel,omitempty"`
+	StripeWidth     int `json:"stripeWidth,omitempty"`
 	// +kubebuilder:validation:Type=string
 	// +kubebuilder:validation:Pattern="^(0|([0-9]+(\\.[0-9]+)?(ns|us|µs|ms|s|m|h))+)$"
 	// +kubebuilder:default="24h"
