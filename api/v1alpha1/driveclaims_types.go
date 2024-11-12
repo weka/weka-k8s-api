f729960710d33f78eb597ee90bf448ce3bdd8c95
Sergey Berezansky <sergey@weka.io>
2024-11-12 12:32:21 +0200

    feat: add more printcolumns to CRDs and ensure -o wide compatibility

--- api/v1alpha1/driveclaims_types.go
+++ api/v1alpha1/driveclaims_types.go
@@ -4,6 +4,10 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Owner",type="string",JSONPath=".spec.owner",description="Owner",priority=0
+// +kubebuilder:printcolumn:name="Device",type="string",JSONPath=".spec.device",description="Device",priority=0
+// +kubebuilder:printcolumn:name="Drive UUID",type="string",JSONPath=".spec.driveUuid",description="Drive UUUD",priority=1
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="Creation time",priority=0
 type DriveClaim struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
