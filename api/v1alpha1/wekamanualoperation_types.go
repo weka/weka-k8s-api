88f9f0a0ff95f8e01e817b19fc20d75f4d145146
Kristina Solovyova <kristina.solovyova@weka.io>
2024-12-05 16:47:51 +0200

    fix: limit SignDrivesPayload to certain type values

--- api/v1alpha1/wekamanualoperation_types.go
+++ api/v1alpha1/wekamanualoperation_types.go
@@ -80,6 +80,7 @@ type EnsureNICsPayload struct {
 }
 
 type SignDrivesPayload struct {
+	// +kubebuilder:validation:Enum=aws-all;device-identifiers;device-paths;all-not-root
 	Type         string            `json:"type"`
 	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 	DevicePaths  []string          `json:"devicePaths,omitempty"`
