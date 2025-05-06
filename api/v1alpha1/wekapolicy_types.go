de9976d0f98de5a04c255f599b638e9544887905
Anton Bykov <tigrawap@gmail.com>
2025-05-06 16:44:27 +0300

    feat: high-level drivers distribution service

--- api/v1alpha1/wekapolicy_types.go
+++ api/v1alpha1/wekapolicy_types.go
@@ -16,15 +16,29 @@ type WekaPolicySpec struct {
 
 // WekaPolicyStatus defines the observed state of WekaPolicy
 type WekaPolicyStatus struct {
-	Status      string      `json:"status"`
-	LastResult  string      `json:"result"`
-	LastRunTime metav1.Time `json:"lastRunTime"`
+	Status      string             `json:"status"`
+	LastResult  string             `json:"result"`
+	LastRunTime metav1.Time        `json:"lastRunTime"`
+	Progress    string             `json:"progress,omitempty"`
+	TypedStatus *TypedPolicyStatus `json:"typedStatus,omitempty"`
+}
+
+// TypedPolicyStatus holds status fields specific to a policy type
+type TypedPolicyStatus struct {
+	DistService *DistServiceStatus `json:"distService,omitempty"`
+	// Add other policy-specific statuses here in the future
+}
+
+// DistServiceStatus holds the status for the enable-local-drivers-distribution policy
+type DistServiceStatus struct {
+	ServiceUrl string `json:"serviceUrl,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Type",type="string",JSONPath=".spec.type",description="Type",priority=0
 // +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.status",description="Status",priority=0
+// +kubebuilder:printcolumn:name="Progress",type="string",JSONPath=".status.progress",description="Policy-specific progress",priority=0
 // +kubebuilder:printcolumn:name="Result",type="string",JSONPath=".status.lastResult",description="Last result",priority=1
 // +kubebuilder:printcolumn:name="Interval",type="string",JSONPath=".spec.payload.interval",description="Interval",priority=1
 // +kubebuilder:printcolumn:name="Last Run Time",type="date",JSONPath=".status.lastRunTime",description="Last run time",priority=1
@@ -47,10 +61,11 @@ type WekaPolicyList struct {
 }
 
 type PolicyPayload struct {
-	SignDrives       *SignDrivesPayload       `json:"signDrivesPayload,omitempty"`
-	SchedulingConfig *SchedulingConfigPayload `json:"schedulingConfigPayload,omitempty"`
-	DiscoverDrives   *DiscoverDrivesPayload   `json:"discoverDrivesPayload,omitempty"`
-	EnsureNICs       *EnsureNICsPayload       `json:"ensureNICsPayload,omitempty"`
+	SignDrives        *SignDrivesPayload       `json:"signDrivesPayload,omitempty"`
+	SchedulingConfig  *SchedulingConfigPayload `json:"schedulingConfigPayload,omitempty"`
+	DiscoverDrives    *DiscoverDrivesPayload   `json:"discoverDrivesPayload,omitempty"`
+	EnsureNICs        *EnsureNICsPayload       `json:"ensureNICsPayload,omitempty"`
+	DriverDistPayload *DriverDistPayload       `json:"driverDistPayload,omitempty"`
 	// +kubebuilder:validation:Type=string
 	// +kubebuilder:validation:Pattern="^(0|([0-9]+(\\.[0-9]+)?(s|m|h))+)$"
 	// +kubebuilder:default="5m"
@@ -59,6 +74,23 @@ type PolicyPayload struct {
 }
 
 type SchedulingConfigPayload struct {
+}
+
+// DriverDistPayload defines the parameters for the enable-local-drivers-distribution policy
+type DriverDistPayload struct {
+	// EnsureImages is a list of Weka images for which drivers should be proactively built.
+	EnsureImages []string `json:"ensureImages,omitempty"`
+	// NodeSelectors is a list of node selectors. Nodes matching any of these selectors will be considered for driver building.
+	// If empty, all nodes in the cluster are considered.
+	NodeSelectors []map[string]string `json:"nodeSelectors,omitempty"`
+	// KernelLabelKey is the custom label key to use for storing the node's kernel version.
+	// If not specified, "weka.io/kernel" will be used.
+	KernelLabelKey *string `json:"kernelLabelKey,omitempty"`
+	// ArchitectureLabelKey is the custom label key to use for storing the node's architecture.
+	// If not specified, "weka.io/architecture" will be used.
+	ArchitectureLabelKey *string `json:"architectureLabelKey,omitempty"`
+	// BuilderPreRunScript is an optional script to run on builder containers after kernel validation.
+	BuilderPreRunScript *string `json:"builderPreRunScript,omitempty"`
 }
 
 func init() {
