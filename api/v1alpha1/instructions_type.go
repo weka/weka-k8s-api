17b4b753d4d360d634167da699752c4e7b729870
Yaron Bar <yaron.bar@weka.io>
2025-09-03 11:18:09 +0300

    fix: add instruction InstructionCopyWekaFilesToDriverLoader

--- api/v1alpha1/instructions_type.go
+++ api/v1alpha1/instructions_type.go
@@ -3,5 +3,6 @@ package v1alpha1
 type InstructionType string
 
 const (
-	InstructionTypeLoadDrivers = "load-drivers"
+	InstructionTypeLoadDrivers             = "load-drivers"
+	InstructionCopyWekaFilesToDriverLoader = "copy-weka-files-to-driver-loader"
 )
